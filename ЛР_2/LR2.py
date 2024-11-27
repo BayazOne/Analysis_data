980d3c69e7127266854e46187007816910a1d4d9
BayazOne <69852007+BayazOne@users.noreply.github.com>
2024-11-27 20:27:47 +0300

    Update LR2.py
    Индивидуальное задание.
    Требуется получить центральность для 50 узлов с «ямой» и «горбиком» значений, т. е. когда последовательность сначала падает вниз, потом возрастает до пикового значения, а затем снова опускается.

--- ЛР_2/LR2.py
+++ ЛР_2/LR2.py
@@ -1,10 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-#Центральность для семи узлов с пиком на конце
-G = nx.path_graph(5)
-G.add_nodes_from([6, 7])
-G.add_edges_from([(4, 7), (5, 7), (6, 7)])
+#Центральность для 50 узлов с «ямой» и «горбиком» значений,
+#т. е. когда последовательность сначала падает вниз, потом возрастает до пикового значения, а затем снова опускается.
+
+G = nx.tadpole_graph(35, 15)
 centrality = nx.eigenvector_centrality_numpy(G)
 for n in centrality:
     print("c(", n, ")=", centrality[n])
